9b24ffc54fcc957d4b7be5f11efbcb07fb4a219d
Lee Aplin <leeaplin@Lees-MacBook-Pro.local>
2019-10-31 15:15:21 +0000

    UPDATE: Sort by time tracked, client owner or name

--- dashboard/helpers.py
+++ dashboard/helpers.py
@@ -1,8 +1,18 @@
 from datetime import date
+from math import ceil
 
 
-def monthFirstDay():
+def month_first_day():
     y, m = date.today().year, date.today().month
     a, m = divmod(m-1, 12)
 
     return date(y+a, m+1, 1)
+
+def week_of_month():
+    today = date.today()
+    first_day = today.replace(day=1)
+
+    dom = today.day
+    adjusted_dom = dom + first_day.weekday()
+
+    return int(ceil(adjusted_dom/7.0))
